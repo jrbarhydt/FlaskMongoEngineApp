54753572f5246b21e3e9b12a5a2ac16b9d0270b8
Johnathon Barhydt <jrbarhydt@gmail.com>
2020-04-05 23:42:16 +0200

    refactor errors to match with jwt defaults

--- FlaskMongo/api/error.py
+++ FlaskMongo/api/error.py
@@ -3,8 +3,17 @@ from flask import jsonify
 
 def invalid_route(e):
     output = {"error":
-              {"message": "404 error: This route is currently not supported. See API documentation."}
+              {"msg": "404 error: This route is currently not supported. See API documentation."}
               }
     resp = jsonify({'result': output})
     resp.status_code = 404
+    return resp
+
+
+def invalid_login():
+    output = {"error":
+              {"msg": "401 error: The email or password provided is invalid."}
+              }
+    resp = jsonify({'result': output})
+    resp.status_code = 401
     return resp
