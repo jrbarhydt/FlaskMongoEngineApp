b98d354b809705b99df33d7484a23ec197037ebe
Johnathon Barhydt <jrbarhydt@gmail.com>
2020-04-05 23:42:35 +0200

    launcher sets env key

--- launch_api.py
+++ launch_api.py
@@ -1,7 +1,10 @@
 import sys
+import os
+
 if __name__ == '__main__':
+    os.environ["ENV_FILE_LOCATION"] = ".env"
     if 'me' in sys.argv:
-        print('--- flask mongo-engine --')
+        print(f'--- flask mongo-engine -- ENV: {os.getenv("ENV_FILE_LOCATION")}')
         from FlaskMongo.MongoEngine import *
     else:
 
