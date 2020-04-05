f2bfb8925e7b5b12099212117d933a97cebdb518
Johnathon Barhydt <jrbarhydt@gmail.com>
2020-04-05 23:43:45 +0200

    added users to api routes

--- FlaskMongo/api/routes.py
+++ FlaskMongo/api/routes.py
@@ -1,5 +1,6 @@
 from .customer import CustomerApi, CustomersApi, RandomCustomersApi
-from .authorization import SignUpApi
+from .authentication import SignUpApi, LoginApi
+from .user import UserApi, UsersApi
 
 
 def create_routes(api):
@@ -7,4 +8,8 @@ def create_routes(api):
     api.add_resource(CustomerApi, '/customer/<customer_id>')
     api.add_resource(RandomCustomersApi, '/customer/rnd/<quantity>')
 
-    api.add_resource(SignUpApi, '/authorization/signup')
+    api.add_resource(SignUpApi, '/authentication/signup')
+    api.add_resource(LoginApi, '/authentication/login')
+
+    api.add_resource(UsersApi, '/user/')
+    api.add_resource(UserApi, '/user/<user_id>')
