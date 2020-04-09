7b31a10cc757917419ede4f5d5593967aefd20d8
Johnathon Barhydt <jrbarhydt@gmail.com>
2020-04-09 22:08:08 +0200

    api launcher has debug mode

--- launch_api.py
+++ launch_api.py
@@ -3,11 +3,17 @@ import os
 
 if __name__ == '__main__':
     os.environ["ENV_FILE_LOCATION"] = ".env"
+    print(sys.argv)
     if 'me' in sys.argv:
         print(f'--- flask mongo-engine -- ENV: {os.getenv("ENV_FILE_LOCATION")}')
         from FlaskMongo.MongoEngine import *
     else:
-
         print("--- flask pymongo ---")
         from FlaskMongo.PyMongo import *
-    app.run(debug=True)
+
+    if 'd' in sys.argv:
+        debug = True
+    else:
+        debug = False
+
+    app.run(debug=debug)
