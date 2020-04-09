2e2e6e989afd74059a25b758ae86bb2df6d0188d
Johnathon Barhydt <jrbarhydt@gmail.com>
2020-04-09 22:08:53 +0200

    order operations added

--- FlaskMongo/api/order.py
+++ FlaskMongo/api/order.py
@@ -2,18 +2,25 @@
 from flask import jsonify
 from flask import request
 from flask import Blueprint
+from flask_restful import Resource
+from flask_jwt_extended import jwt_required, get_jwt_identity
 # mongo-engine classes
 from MongoEngineDB.Models.Orders import Order
-
+from MongoEngineDB.Models.Users import User
 
 order = Blueprint('order', __name__, url_prefix='/order')
 
 
+class OrdersApi(Resource):
+    def get(self):
+        return jsonify({'result': Order.objects()})
+
+
 # /order
-@order.route('/', methods=['GET'])
-def get_all_orders():
-    output = Order.objects()
-    return jsonify({'result': output})
+# @order.route('/', methods=['GET'])
+# def get_all_orders():
+#     output = Order.objects()
+#     return jsonify({'result': output})
 
 
 @order.route('/<order_id>', methods=['GET'])
@@ -22,12 +29,16 @@ def get_one_order(order_id):
 
 
 @order.route('/', methods=['POST'])
+@jwt_required
 def add_order():
-    data = request.get_json()
-    post_order = Order(**data).save()
-    output = {'id': str(post_order.id)}
-    return jsonify({'result': output})
-
+    authorized_user = User.objects(id=get_jwt_identity(), access__server=True)
+    if authorized_user:
+        data = request.get_json()
+        post_order = Order(**data).save()
+        output = {'id': str(post_order.id)}
+        return jsonify({'result': output})
+    else:
+        return jsonify({"message": "Unauthorized User"})
 
 @order.route('/<order_id>', methods=['PUT'])
 def modify_order(order_id):
