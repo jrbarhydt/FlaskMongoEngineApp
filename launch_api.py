970518985a722bbad6de29788338cd3e39e0fd5e
Johnathon Barhydt <jrbarhydt@gmail.com>
2020-04-10 03:54:36 +0200

    now launch with gunicorn launch_api:launch_me

--- launch_api.py
+++ launch_api.py
@@ -1,19 +1,14 @@
 import sys
 import os
 
-if __name__ == '__main__':
-    os.environ["ENV_FILE_LOCATION"] = ".env"
-    print(sys.argv)
-    if 'me' in sys.argv:
-        print(f'--- flask mongo-engine -- ENV: {os.getenv("ENV_FILE_LOCATION")}')
-        from FlaskMongo.MongoEngine import *
-    else:
-        print("--- flask pymongo ---")
-        from FlaskMongo.PyMongo import *
 
-    if 'd' in sys.argv:
-        debug = True
-    else:
-        debug = False
+def launch_me():
+    from FlaskMongo.MongoEngine import *
+    print(f'--- flask mongo-engine -- ENV: {os.getenv("ENV_FILE_LOCATION")}')
+    return app
 
-    app.run(debug=debug)
+
+def launch_pymongo():
+    from FlaskMongo.PyMongo import *
+    print("--- flask pymongo ---")
+
