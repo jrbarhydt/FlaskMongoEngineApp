4ceb1387f7745414cf5b53716846ea260417d3fa
Johnathon Barhydt <jrbarhydt@gmail.com>
2020-04-17 23:02:34 +0200

    added multiple entry pints, now can be launched via wsgi server, or allow flask to serve (in which case app is run in debug mode, and pymongo / mongoengine can be chosen from sys args)

--- launch_api.py
+++ launch_api.py
@@ -2,13 +2,24 @@ import sys
 import os
 
 
-def launch_me():
-    from FlaskMongo.MongoEngine import *
-    print(f'--- flask mongo-engine -- ENV: {os.getenv("ENV_FILE_LOCATION")}')
+def launch():
+    # entry point for wsgi server, returns flask app
+    from FlaskMongo.MongoEngine import app
     return app
 
 
-def launch_pymongo():
-    from FlaskMongo.PyMongo import *
-    print("--- flask pymongo ---")
+if __name__ == '__main__':
+    # This is true if not launched by wsgi server (gunicorn), thus app is launched in debug mode
 
+    os.environ["ENV_FILE_LOCATION"] = ".env"
+
+    if 'pym' in sys.argv:
+        # Run from pymongo
+        from FlaskMongo.PyMongo import *
+        print(f'--- flask pymongo -- ENV: {os.getenv("ENV_FILE_LOCATION")}')
+    else:
+        # run from mongoengine
+        print(f'--- flask mongo-engine -- ENV: {os.getenv("ENV_FILE_LOCATION")}')
+        from FlaskMongo.MongoEngine import *
+
+    app.run(debug=True)
